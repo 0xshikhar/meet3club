6d25cd4c82f4003c84e00746b46b267437379ddf
linux369 <shikhar@iiitmanipur.ac.in>
2023-02-09 23:05:37 +0530

    router update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css';
 import Home from './home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Match from './match'
 import Meeting from './meeting'
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
           <Route index element={<Home />} />
-          <Route path="/meeting" caseSensitive={false} element={<Meeting />} />
-          <Route path="/match" caseSensitive={false} element={<Match />} />
+          <Route index exact path="/meeting"  element={<Meeting />} />
+          <Route index exact path="/match" element={<Match />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { chunkSizeWarningLimit: 3600, },
+  build: {
+    chunkSizeWarningLimit: 3600,
+  },
 
 
 })

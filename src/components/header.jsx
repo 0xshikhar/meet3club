be179f5209d15691a7c0021dc4479e0e60bc00ee
linux369 <shikhar@iiitmanipur.ac.in>
2023-02-09 20:55:35 +0530

    match page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -210,7 +210,7 @@ export default function Header() {
                         </li>
 
                         <li>
-                            <Link to="/match" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Meeting</Link>
+                            <Link to="/match" className="block py-2 pl-3 pr-4 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Match</Link>
                         </li>
                     </ul>
                 </div>
